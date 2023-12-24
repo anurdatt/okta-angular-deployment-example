41e926a7deb4f83341888287d46ed92e1cb4ed13
Anuran Datta <anuran.datta@hotmail.com>
2023-12-24 15:09:19 +0530

    Update Okta OIDC Configuration

--- notes/src/app/auth-routing.module.ts
+++ notes/src/app/auth-routing.module.ts
@@ -8,9 +8,9 @@ import { OKTA_CONFIG, OktaAuthModule } from '@okta/okta-angular';
 import { AuthInterceptor } from './shared/okta/auth.interceptor';
 
 const oktaConfig = {
-  issuer: 'https://dev-169032.okta.com/oauth2/default',
+  issuer: 'https://dev-76789052.okta.com/oauth2/default',
   redirectUri: window.location.origin + '/callback',
-  clientId: '0oad7uhq9l3u4RoaC4x6',
+  clientId: '0oae3aix2gO5RuAev5d7',
   pkce: true
 };
 
